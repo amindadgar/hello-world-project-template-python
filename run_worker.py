665a3f40eeccc478a3b96186613223743d794d29
Mohammad Amin <dadgaramin96@gmail.com>
2025-05-14 11:00:14 +0330

    feat: add logging for Temporal server connection in run_worker.py

--- run_worker.py
+++ run_worker.py
@@ -1,4 +1,5 @@
 # @@@SNIPSTART python-project-template-run-worker
+import logging
 import asyncio
 
 from temporalio import activity, workflow
@@ -9,12 +10,17 @@ from activities import say_hello
 from workflows import SayHello
 from config import TEMPORAL_HOST, TEMPORAL_NAMESPACE, TASK_QUEUE, TLS_CONFIG
 
+logging.basicConfig(level=logging.INFO)
+
 async def main():
     client = await Client.connect(
         TEMPORAL_HOST,
         namespace=TEMPORAL_NAMESPACE,
         tls=TLS_CONFIG if TLS_CONFIG else False,
     )
+
+    logging.info(f"Connected to Temporal server at {TEMPORAL_HOST}")
+
     # Run the worker
     worker = Worker(
         client, task_queue=TASK_QUEUE, workflows=[SayHello], activities=[say_hello]
